1a70f7588796b5cb1702e274a9f435992620043c
Nioqui <Nicolas.l-19@outlook.com>
2024-06-17 21:37:48 -0300

    close to finish the social media pills

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import './GlobalStyles.css'
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Smpills from './components/Smpills';
-
 
 
 function App() {
@@ -11,7 +9,6 @@ function App() {
     <>
       <Header />
       <Hero />
-      <Smpills />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
 
+
 const NavBar = styled.nav`
   display: flex;
   flex-direction: row;
