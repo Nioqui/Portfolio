ac544f97233bb46f5caec757a4ef9c7407713bc8
Nioqui <Nicolas.l-19@outlook.com>
2024-07-01 21:19:39 -0300

    new things in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import styled from 'styled-components';
+import about from '../assets/images/about.png';
 
 const AboutSection = styled.section`
     width: 100%;
     height: 50vh;
-    background-color: red;
+    background-color: #474747;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+  .container{
+    width: 60%;
+    display: flex;
+
+  }
+
+    img{
+      width: 280px;
+      height: auto;
+    }
+
+    .text{
+      width: 60%;
+      margin: 10px;
+    }
 `
 
 function About() {
   return (
     <AboutSection>
-        <h2>So, who am I?</h2>
-        <span>Fullstack dev</span>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo repudiandae dolor doloribus cumque eos, sunt velit vitae facilis adipisci expedita a possimus! Et doloremque ab fugit eligendi placeat nisi neque!</p>
+      <div className='container'>
+        <div className='text'>
+            <h2>So, who am I?</h2>
+            <span>Fullstack dev</span>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo repudiandae dolor doloribus cumque eos, sunt velit vitae facilis adipisci expedita a possimus! Et doloremque ab fugit eligendi placeat nisi neque!</p>
+        </div>
+          <div className='img'>
+            <img src={about}/>
+          </div>
+      </div>
     </AboutSection>
   )
 }
